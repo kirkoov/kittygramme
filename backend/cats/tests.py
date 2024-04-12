ac23d2e6f686dc80731ceff8d6834e4763a2448c
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-04-12 18:31:48 +0300

    Carry on with the yaml file.

--- backend/cats/tests.py
+++ backend/cats/tests.py
@@ -4,15 +4,16 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+User = get_user_model()
+
 
 class CatsAPITestCase(TestCase):
     def setUp(self):
-        User = get_user_model()
         self.user = User.objects.create_user(username="auth_user")
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """Check the cats page is 200 OK."""
         response = self.client.get("/api/cats/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
